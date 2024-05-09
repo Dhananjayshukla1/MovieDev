854ff41ac72283fd11b347ad5f55da077d327e8d
DHANANJAY SHUKLA <dhananjayshukla199@gmail.com>
2024-05-09 21:34:24 +0530

    "feat: "

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 const App = () => {
   return (
-    <div>
-      
+    <div className='
+    w-screen h-screen'>
+    <Routes>
+      <Route path='/' element={<Home/>}>
+      </Route>
+    
+    
+    </Routes>
     </div>
+    
   )
 }
 
